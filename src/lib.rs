b8468cba3d12596e7782fdf26bdc163d58c67899
Enoki <enokiun@gmail.com>
2022-12-01 01:42:54 +0300

    docs: document eludrs::todel

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ mod models;
 pub use gateway::{Events, GatewayClient, GATEWAY_URL};
 pub use http::{HttpClient, REST_URL};
 
+/// All the todel models re-exported
 pub mod todel {
     pub use todel::models::*;
 }
