b749dd8a021e064d5b5f8887bc37421b5e83f003
Enoki <enokiun@gmail.com>
2022-12-05 17:39:32 +0300

    chore: add crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,50 @@
-//! A simple rust library for interacting with the Eludris API
+//! # Eludrs
+//!
+//! A simple asynchronous wrapper for the Eludris API
+//!
+//! ## Installation
+//!
+//! Just run the following command:
+//!
+//! ```sh
+//! cargo add --git https://github.com/eludris-community/eludrs
+//! ```
+//!
+//! > **Note**
+//! > You may be wondering why this is not on crates.io, that's because Eludris is
+//! > still in early development stages, expect a release when Eludris is more stable.
+//!
+//! ## Example
+//!
+//! While an API wrapper has many uses, here's an example of what most people will
+//! end up using this for, making Bots:
+//!
+//! ```rust
+//! use eludrs::HttpClient;
+//! use futures::stream::StreamExt;
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let mut http = HttpClient::new().name("Uwuki".to_string());
+//!     let gateway = http.create_gateway.await?;
+//!     let mut events = gateway.get_events().await.unwrap();
+//!
+//!     while let Some(msg) = events.next().await {
+//!         if msg.content == "!ping" {
+//!             http.send("Pong").await.unwrap();
+//!         }
+//!     }
+//! }
+//! ```
+//!
+//! ## Docs
+//!
+//! If you want documentation you can currently get that by going to your project
+//! and running
+//!
+//! ```shell
+//! cargo doc -p eludrs --open
+//! ```
 mod gateway;
 mod http;
 mod models;
